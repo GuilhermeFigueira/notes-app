36cc3aeac180c4bd1a7046b29f6b7f5c6059fdfa
Guilherme Figueira <guibf2004@gmail.com>
2024-03-16 14:32:59 -0300

    adiciona varias funcoes

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const createNoteForm = document.getElementById("createNoteForm");
 const noteTextInput = document.getElementById("noteText");
 const noteTitleInput = document.getElementById("noteTitle");
+const notesDiv = document.getElementById("notes");
 
 const noteHandler = createNoteHandler();
+
+noteHandler.subscribe(renderItems);
+noteHandler.subscribe(saveChangesOnBrowser);
+
+document.addEventListener("DOMContentLoaded", () => {
+	const notes = loadFromBrowser("notes");
+	if (Object.keys(notes).length) {
+		noteHandler.updateState(notes);
+	}
+});
 
 createNoteForm.addEventListener("submit", (e) => {
 	e.preventDefault();
 	noteTitle = noteTitleInput.value;
 	noteText = noteTextInput.value;
-	noteHandler.createNote({ text: noteText, title: noteTitle });
+	if (noteTitle && noteText) {
+		noteHandler.createNote({ text: noteText, title: noteTitle });
+	}
 });
-
-noteHandler.subscribe(saveChangesOnBrowser);
 
 function createNoteHandler() {
 	let observers = [];
@@ -30,7 +41,12 @@ function createNoteHandler() {
 		}
 	}
 
-	const notes = {};
+	let notes = [];
+
+	function updateState(newNotes) {
+		Object.assign(notes, newNotes);
+		notifyAll(notes);
+	}
 
 	function createNote(note) {
 		const title = note.title;
@@ -41,29 +57,34 @@ function createNoteHandler() {
 		const year = date.getFullYear();
 		const hours = date.getHours();
 		const minutes = date.getMinutes();
+		const seconds = date.getSeconds();
 
-		const dateCreated = `${day}-${month}-${year}-${hours}-${minutes}`;
+		const dateCreated = `${day}-${month}-${year}-${hours}-${minutes}-${seconds}`;
 		const noteId = `${dateCreated}_${Math.floor(Math.random() * 10000)}`;
 
-		notes[noteId] = {
+		notes.push({
+			noteId: noteId,
 			title: title,
 			text: text,
 			date: dateCreated,
-		};
-
+		});
 		notifyAll(notes);
 	}
 
-	function renderNote(note) {}
-	function deleteNote(noteId) {}
+	function deleteNote(noteId) {
+		notes = notes.filter((note) => {
+			return note.noteId !== noteId;
+		});
+		notifyAll(notes);
+	}
 	function editNote(noteId) {}
 
 	return {
 		createNote,
-		renderNote,
 		deleteNote,
 		editNote,
 		subscribe,
+		updateState,
 	};
 }
 function saveChangesOnBrowser(object) {
@@ -71,6 +92,36 @@ function saveChangesOnBrowser(object) {
 	object = JSON.stringify(object);
 	localStorage.setItem("notes", object);
 }
-function loadSaveBrowser() {
-	const notesss = JSON.parse(localStorage.getItem("notes"));
+
+function loadFromBrowser(itemName) {
+	const notes = JSON.parse(localStorage.getItem(itemName));
+	return { ...notes };
+}
+
+function renderItems(items) {
+	notesDiv.innerHTML = "";
+	items.forEach((item) => {
+		const title = item.title;
+		const text = item.text;
+		const itemDiv = document.createElement("div");
+		itemDiv.id = item.noteId;
+		itemDiv.classList.add("note");
+		const itemTitleHTML = document.createElement("h1");
+		const itemTextHTML = document.createElement("p");
+		const deleteItemButton = document.createElement("button");
+		const itemDate = document.createElement("span");
+		itemDate.textContent = item.date;
+		deleteItemButton.addEventListener("click", () =>
+			noteHandler.deleteNote(item.noteId)
+		);
+		deleteItemButton.textContent = "Delete Note";
+		deleteItemButton.classList.add("deleteButton");
+		itemTitleHTML.textContent = title;
+		itemTextHTML.textContent = text;
+		notesDiv.appendChild(itemDiv);
+		itemDiv.appendChild(itemTitleHTML);
+		itemDiv.appendChild(itemTextHTML);
+		itemDiv.appendChild(itemDate);
+		itemDiv.appendChild(deleteItemButton);
+	});
 }
--- readme.md
+++ readme.md
@@ -6,15 +6,15 @@ Create and store your notes for later purpose!
 
 ## User Stories
 
-- [ ] User can create a note
+- [x] User can create a note
 - [ ] User can edit a note
-- [ ] User can delete a note
-- [ ] When closing the browser window the notes will be stored and when the User returns, the data will be retrieved
+- [x] User can delete a note
+- [x] When closing the browser window the notes will be stored and when the User returns, the data will be retrieved
 
 ## Bonus features
 
 - [ ] User can create and edit a note in Markdown format. On save it will convert Markdown to HTML
-- [ ] User can see the date when he created the note
+- [x] User can see the date when he created the note
 
 ## Useful links and resources
 
